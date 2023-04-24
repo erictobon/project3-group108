00197d9fd3cb4bc78093fef9cfdbbec416761466
Evan Tobon <evantobon@ufl.edu>
2023-04-24 18:01:46 -0400

    recent

---
+++ carrier.h
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Carrier{
+    public:
+        string airCode;
+        string airName;
+        int month;
+        int year;
+        int carrierNum;
+        int lateNum;
+        int aviationNum;
+        int securityNum;
+        int weatherNum;
+        int delaysNum;
+        int flightsNum;
+        int carrierMins;
+        int lateMins;
+        int aviationMins;
+        int securityMins;
+        int totalMins;
+        int weatherMins;
+
+   // public:
+   //     void setAirCode(string airCode){
+   //         this->airCode = airCode;
+   //     }
+   //     string getAirCode(){
+   //         return airCode;
+   //     }
+   //     void setAirName(string airName){
+   //         this->airName = airName;
+   //     }
+   //     string getAirName(){
+   //         return airName;
+   //     }
+   //     void setCarrierNum(int carrierNum){
+   //         this->carrierNum = carrierNum;
+   //     }
+   //     int getCarrierNum(){
+   //         return carrierNum;
+   //     }
+   //     void setLateNum(int lateNum){
+   //         this->lateNum = lateNum;
+   //     }
+   //     int getLateNum(){
+   //         return lateNum;
+   //     }
+   //     void setAviationNum(int aviationNum){
+   //         this->aviationNum = aviationNum;
+   //     }
+   //     int getAviationNum(){
+   //         return aviationNum;
+   //     }
+//
+
+
+};
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <fstream>
 #include <sstream>
+#include "carrier.h"
 using namespace std;
 
 int main(){
@@ -46,49 +47,70 @@ int main(){
     string firstLine;
     getline(inputFile, firstLine);
     string fileLine;
+    vector<Carrier*> data;
     while(getline(inputFile, fileLine)){
+        Carrier* newData;
         istringstream stream(fileLine);
         getline(stream, airCode, ',');
+        newData->airCode = airCode;
         getline(stream, airName, ',');
+        newData->airName = airName;
         getline(stream, buffer, ',');
         getline(stream, month, ',');
         monthNum = stoi(month);
+        newData->month = monthNum;
         getline(stream, buffer, ',');
         getline(stream, year, ',');
         yearNum = stoi(year);
+        newData->year = yearNum;
         getline(stream, carrier, ',');
         carrierNum = stoi(carrier);
+        newData->carrierNum = carrierNum;
         getline(stream, latePlane, ',');
         lateNum = stoi(latePlane);
+        newData->lateNum = lateNum;
         getline(stream, aviationSystem, ',');
         aviationNum = stoi(aviationSystem);
+        newData->aviationNum = aviationNum;
         getline(stream, security, ',');
         securityNum = stoi(security);
+        newData->securityNum = securityNum;
         getline(stream, weather, ',');
         weatherNum = stoi(weather);
+        newData->weatherNum = weatherNum;
         getline(stream, buffer, ',');
         getline(stream, buffer, ',');
         getline(stream, buffer, ',');
         getline(stream, totalDelays, ',');
         delaysNum = stoi(totalDelays);
+        newData->delaysNum = delaysNum;
         getline(stream, buffer, ',');
         getline(stream, buffer, ',');
         getline(stream, totalFlights, ',');
         flightsNum = stoi(totalFlights);
+        newData->flightsNum = flightsNum;
         getline(stream, carrierMinutes, ',');
         carrierMins = stoi(carrierMinutes);
+        newData->carrierMins = carrierMins;
         getline(stream, latePlaneMinutes, ',');
         lateMins = stoi(latePlaneMinutes);
+        newData->lateMins = lateMins;
         getline(stream, aviationSystemMinutes, ',');
         aviationMins = stoi(aviationSystemMinutes);
+        newData->aviationMins = aviationMins;
         getline(stream, securityMinutes, ',');
         securityMins = stoi(securityMinutes);
+        newData->securityMins = securityMins;
         getline(stream, totalMinutes, ',');
         totalMins = stoi(totalMinutes);
+        newData->totalMins = totalMins;
         getline(stream, weatherMinutes, ',');
         weatherMins = stoi(weatherMinutes);
-        
+        newData->weatherMins = weatherMins;
+        data.push_back(newData);
     }
     //29 different airports, 20 years of monthly data
     inputFile.close();
+
+    
 }
