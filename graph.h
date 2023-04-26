5ecddc0108958ac468b87d41afdf283b6c24f673
Evan Tobon <evantobon@ufl.edu>
2023-04-26 18:08:04 -0400

    recent

--- graph.h
+++ graph.h
@@ -9,12 +9,19 @@ using namespace std;
 class Graph{
     private:
         map<string, vector<pair<string, int>>> graph;
+        map<string, vector<pair<string, int>>> carrierGraph;
+        map<string, vector<pair<string, int>>> lateGraph;
+        map<string, vector<pair<string, int>>> systemGraph;
+        map<string, vector<pair<string, int>>> securityGraph;
+        map<string, vector<pair<string, int>>> weatherGraph;
+
 
     public:
         //void insertEdge(string from, string to);
         void insert(vector<Carrier*>& data, int choice);
         void getDataOne();
         void getDataTwo();
+        void getDataFour();
         void getDataThree();
         string date(int month, int year);
 };
@@ -37,9 +44,13 @@ void Graph::insert(vector<Carrier*>& data, int choice){
         }
     }
     if(choice == 3){
-        for(int i = 0; i < data.size() - 28; i += 28){
-            for(int j = i; j < i + 28; j++){
-                graph[date(data[i]->month, data[i]->year)].push_back(make_pair(data[j]->airCode, data[j]->carrierNum));
+        for(int i = 0; i < data.size() - 29; i += 29){
+            for(int j = i; j < i + 29; j++){
+                carrierGraph[date(data[i]->month, data[i]->year)].push_back(make_pair(data[j]->airCode, data[j]->carrierNum));
+                lateGraph[date(data[i]->month, data[i]->year)].push_back(make_pair(data[j]->airCode, data[j]->lateNum));
+                systemGraph[date(data[i]->month, data[i]->year)].push_back(make_pair(data[j]->airCode, data[j]->aviationNum));
+                securityGraph[date(data[i]->month, data[i]->year)].push_back(make_pair(data[j]->airCode, data[j]->securityNum));
+                weatherGraph[date(data[i]->month, data[i]->year)].push_back(make_pair(data[j]->airCode, data[j]->weatherNum));
             }
         }
     }
@@ -66,13 +77,47 @@ void Graph::getDataTwo(){
     }
 }
 
-void Graph::getDataThree(){
-    for(auto iter = graph.begin(); iter != graph.end(); iter++){
-        cout << iter->first << endl;
-        for(int j = 0; j < graph[iter->first].size(); j++){
-            cout << iter->second[j].first << ": " << iter->second[j].second << endl;
+void Graph::getDataFour(){
+    int totalCarrier;
+    int totalLate;
+    int totalAviation;
+    int totalSecurity;
+    int totalWeather;
+    for(auto iter = carrierGraph.begin(); iter != carrierGraph.end(); iter++){
+        for(int j = 0; j < carrierGraph[iter->first].size(); j++){
+            totalCarrier += iter->second[j].second;
         }
     }
+    for(auto iter = lateGraph.begin(); iter != lateGraph.end(); iter++){
+        for(int j = 0; j < lateGraph[iter->first].size(); j++){
+            totalLate += iter->second[j].second;
+        }
+    }
+    for(auto iter = systemGraph.begin(); iter != systemGraph.end(); iter++){
+        for(int j = 0; j < systemGraph[iter->first].size(); j++){
+            totalAviation += iter->second[j].second;
+        }
+    }
+    for(auto iter = securityGraph.begin(); iter != securityGraph.end(); iter++){
+        for(int j = 0; j < securityGraph[iter->first].size(); j++){
+            totalSecurity += iter->second[j].second;
+        }
+    }
+    for(auto iter = weatherGraph.begin(); iter != weatherGraph.end(); iter++){
+        for(int j = 0; j < weatherGraph[iter->first].size(); j++){
+            totalWeather += iter->second[j].second;
+        }
+    }
+
+    cout << "Delays by Carrier: " << totalCarrier << endl;
+    cout << "Delays by Late Aircraft: " << totalLate << endl;
+    cout << "Delays by National Aviation System: " << totalAviation << endl;
+    cout << "Delays by Security: " << totalSecurity << endl;
+    cout << "Delays by Weather: " << totalWeather << endl;
+}
+
+void Graph::getDataThree(){
+
 }
 
 string Graph::date(int month, int year){
